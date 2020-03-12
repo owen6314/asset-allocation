9b7097ff315dbed8fa1d589c02932caed289b7e7
owen <thss15_zhangqr@163.com>
2020-03-12 00:12:35 -0400

    update data processing code for creating data for model 2

---
+++ data/process/createcsv.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+from datetime import datetime
+import time
+
+root_dir = 'sample2/'
+result_file = 'all.csv'
+counter = 0
+
+result_df = pd.DataFrame(columns=['DateTime'])
+
+for file in os.listdir(root_dir):
+    if '.csv' in file:
+        print("processing: " + file)
+        stock_name = file[:-4]
+        df = pd.read_csv(root_dir + file)
+        df = df[['Date', 'Close']]
+        if len(df) != 1258:
+            continue
+        if len(result_df) == 0:
+            result_df['DateTime'] = df['Date']
+            f = lambda x: datetime.strptime(x, "%Y-%m-%d").strftime('%Y%m%d')
+            result_df['DateTime'] = result_df['DateTime'].apply(f)
+        result_df[stock_name] = df['Close']
+        counter += 1
+
+print("Total file number: " + str(counter))
+result_df.to_csv(result_file, index=False)
+
--- data/process/createdb.py
+++ data/process/createdb.py
@@ -14,13 +14,18 @@ pd.set_option('display.max_colwidth', None)
 
 root_dir = 'sample/'
 new_dir = 'new_sample/'
+counter = 0
 for file in os.listdir(root_dir):
     if '.csv' in file:
         print("processing: " + file)
         df = pd.read_csv(root_dir + file)
         df = df[['Date', 'Name', 'High', 'Low', 'Open', 'Close', 'Volume']]
+        if len(df) != 1258:
+            continue
+        counter += 1
         f = lambda x: time.mktime(datetime.strptime(x, "%Y-%m-%d").timetuple())
         df['Date'] = df['Date'].apply(f)
         df = df.astype({'Date': 'int'})
         df.to_sql('history', conn, if_exists='append', index=False)
         # df.to_csv(new_dir + file[:-4] + "_new.csv", index=False)
+print("Total file number: " + str(counter))
