1fe6a1ff65cdbf9c87f95c65434703266c709dfa
Jackie Lin <little.carol@163.com>
2020-04-26 21:32:04 -0400

    Update training & testing epoch

--- ref/DQN/wrap.sh
+++ ref/DQN/wrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python3 run.py --mode train -i $1 -e 10 > log.txt
+python3 run.py --mode train -i $1 > log.txt
 weights=$(tail -6 log.txt | head -1)
 echo $weights 
-python3 run.py --mode test --weights $weights -e 20 > log.txt
+python3 run.py --mode test --weights $weights -e 200 > log.txt
 python3 get_result.py
